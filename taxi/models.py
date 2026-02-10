1a86813ac7bb12901741fa470deb411dcf179fc4
Dmytro <ys33ys33ys55@gmail.com>
2026-02-10 16:36:34 +0100

    Fixed

--- taxi/models.py
+++ taxi/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 
 class Manufacturer(models.Model):
@@ -16,4 +17,4 @@ class Car(models.Model):
     manufacturer = models.ForeignKey(
         Manufacturer, on_delete=models.CASCADE, related_name="cars"
     )
-    drivers = models.ManyToManyField(Driver, related_name="cars")
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="cars")
